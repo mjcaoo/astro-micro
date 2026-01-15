b801a683c62aa33f0ab6e3ee5f1edb2a44ef3321
mjcaoo <mjcaoo@qq.com>
2026-01-15 20:16:01 +0800

    feat: add new blog posts on Semantic Versioning, colcon usage, Nuwa-HP60C camera, and YDLIDAR T-mini Plus; update project details and remove obsolete projects

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "Mjcaoo's Blog",
+  DESCRIPTION: "A personal blog about technology, programming, and life.",
+  EMAIL: "mjcaoo@qq.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "A personal blog about technology, programming, and life.",
 };
 
 export const BLOG: Metadata = {
@@ -26,15 +26,11 @@ export const PROJECTS: Metadata = {
 
 export const SOCIALS: Socials = [
   {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheek",
-  },
-  {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/mjcaoo",
   },
   {
     NAME: "Website",
-    HREF: "https://trevortylerlee.com",
+    HREF: "https://kpahead.cn",
   },
 ];
